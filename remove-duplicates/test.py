37c7c5dfae0fb3cb10930e8d837188fce184b3c3
Parimala <pari.banda17@gmail.com>
2021-07-19 17:04:55 +1000

    Initial commit

---
+++ remove-duplicates/test.py
@@ -0,0 +1,16 @@
+# Test case created based on test case (custom judge) used on LeetCode
+import unittest
+from remove_duplicates import Solution
+
+class removeDuplicatesTestCase(unittest.TestCase):
+    
+    def test_duplicates(self):
+        a = [1,1,2]
+        self.assertEqual(Solution().removeDuplicates(a), 2, "Expected length is 2")
+       
+       
+        
+        
+if __name__ == '__main__':
+    unittest.main()
+    

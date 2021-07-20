166a9095ec429f2d5a2975cd9a3519e3160391db
Parimala <pari.banda17@gmail.com>
2021-07-20 19:16:55 +1000

    Initial Commit

---
+++ unique-char/first_unique_char.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """          
+        frequency = Counter(s)
+
+        for i, char in enumerate (s):
+            if frequency[char] == 1:
+                print("Index of the non-repeating charater of the input string is:",i)
+                return i     
+        return -1
+     
+            

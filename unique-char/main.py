3fdb68d77f88bec54cc639b0db215d50bff24df8
Parimala <pari.banda17@gmail.com>
2021-07-20 19:18:41 +1000

    Initial commit

---
+++ unique-char/main.py
@@ -0,0 +1,7 @@
+from first_unique_char import Solution
+
+class main:
+    s = input("Enter input string:")
+    Solution().firstUniqChar(s)
+    
+main()
---
+++ unique-char/test.py
@@ -0,0 +1,16 @@
+# Test case created based on test case used on LeetCode
+import unittest
+from first_unique_char import Solution
+
+class uniqCharTestCase(unittest.TestCase):
+    
+    def test_uniqChar(self):
+        self.assertEqual(Solution().firstUniqChar("Hello"), 0, "Expected index number is 0")
+        self.assertEqual(Solution().firstUniqChar("loveletter"), 1, "Expected index number is 1")
+        self.assertEqual(Solution().firstUniqChar("llama"), 3, "Expected index number is 3")
+        self.assertEqual(Solution().firstUniqChar("xxyy"), -1, "Expected index number is -1")
+       
+        
+if __name__ == '__main__':
+    unittest.main()
+    

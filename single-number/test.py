a3e29f4e139bd3bb2ddb2d0ce4f99b6e5e37f2e8
Parimala <pari.banda17@gmail.com>
2021-07-26 22:09:23 +1000

    Initial commit

---
+++ single-number/test.py
@@ -0,0 +1,15 @@
+# Test case created based on test case used on LeetCode
+import unittest
+from single_number import Solution
+
+class singleNumberTestCase(unittest.TestCase):
+    
+    def test_uniqChar(self):
+        self.assertEqual(Solution().singleNumber([2,2,1]), 1, "Expected number is 1")
+        self.assertEqual(Solution().singleNumber([4,1,2,1,2]), 4, "Expected number is 4")
+        self.assertEqual(Solution().singleNumber([1]), 1, "Expected number is 1")
+        self.assertEqual(Solution().singleNumber([-89,4,4,3,3,6,6]), -89, "Expected number is -89")
+       
+        
+if __name__ == '__main__':
+    unittest.main()

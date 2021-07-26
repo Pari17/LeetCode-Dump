2ea39cd8e0805fe00ebee40d3d02ef799bf116e7
Parimala <pari.banda17@gmail.com>
2021-07-26 22:09:04 +1000

    Initial commit

---
+++ single-number/main.py
@@ -0,0 +1,8 @@
+from single_number import Solution
+
+class main:
+    print("Rule: Each element in the array appears twice except for one element which appears only once")
+    val = input("Enter numbers that abide condition above: ")
+    Solution().singleNumber(val)
+    
+main()

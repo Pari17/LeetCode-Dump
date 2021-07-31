0edbcf4c5240836c2c45986538ef4cbd1dd9fdf3
Parimala <pari.banda17@gmail.com>
2021-07-31 23:12:08 +1000

    Initial commit

---
+++ valid-palindrome/palindrome.py
@@ -0,0 +1,14 @@
+import re
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """   
+        str1 = re.sub(r'[\W_]+', '', s).lower()
+        str2 = str1[::-1].lower()
+        if str1 == str2:
+            print("True")
+            return 1
+        print("False")
+        return 0

a9dc94b97fab17cdb4016926b1d2cf91c9cd86f6
Parimala Banda <86640638+Pari17@users.noreply.github.com>
2021-08-04 22:09:32 +1000

    Update test.py

--- single-number/test.py
+++ single-number/test.py
@@ -8,8 +8,7 @@ class singleNumberTestCase(unittest.TestCase):
         self.assertEqual(Solution().singleNumber([2,2,1]), 1, "Expected number is 1")
         self.assertEqual(Solution().singleNumber([4,1,2,1,2]), 4, "Expected number is 4")
         self.assertEqual(Solution().singleNumber([1]), 1, "Expected number is 1")
-        self.assertEqual(Solution().singleNumber([-89,4,4,3,3,6,6]), -89, "Expected number is -89")
-       
+        self.assertEqual(Solution().singleNumber([-89,4,4,3,3,6,6]), -89, "Expected number is -89")  
         
 if __name__ == '__main__':
     unittest.main()

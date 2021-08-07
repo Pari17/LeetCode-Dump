bb5f53a2aae3839447cab0509158780033307783
Parimala <pari.banda17@gmail.com>
2021-08-07 20:29:31 +1000

    Update file

--- roman-to-integer/test.py
+++ roman-to-integer/test.py
@@ -8,7 +8,7 @@ class romanTestCase(unittest.TestCase):
         self.assertEqual(Solution().romanToInt("IV"), 4, "Expected index number is 4")
         self.assertEqual(Solution().romanToInt("IX"), 9, "Expected index number is 9")
         self.assertEqual(Solution().romanToInt("LVIII"), 58, "Expected index number is 58")
-        self.assertEqual(Solution().romanToInt("MCMXCIV"), 58, "Expected index number is 58")
+        self.assertEqual(Solution().romanToInt("MCMXCIV"), 1994, "Expected index number is 1994")
  
         
 if __name__ == '__main__':

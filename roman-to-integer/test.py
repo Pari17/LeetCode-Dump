9a5808e755ceded2d75cda7666ed5b7f9562996a
Parimala <pari.banda17@gmail.com>
2021-08-07 20:19:24 +1000

    Initial commit

---
+++ roman-to-integer/test.py
@@ -0,0 +1,16 @@
+# Test case created based on test case used on LeetCode
+import unittest
+from roman import Solution
+
+class uniqCharTestCase(unittest.TestCase):
+    
+    def test_roman(self):
+        self.assertEqual(Solution().romanToInt("IV"), 4, "Expected index number is 4")
+        self.assertEqual(Solution().romanToInt("IX"), 9, "Expected index number is 9")
+        self.assertEqual(Solution().romanToInt("LVIII"), 58, "Expected index number is 58")
+        self.assertEqual(Solution().romanToInt("MCMXCIV"), 58, "Expected index number is 58")
+ 
+        
+if __name__ == '__main__':
+    unittest.main()
+    

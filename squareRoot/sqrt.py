c65a8be3792ffa39e68b3ecf6a49f561cc2bad1b
Parimala <pari.banda17@gmail.com>
2021-08-10 17:46:24 +1000

    Initial commit

---
+++ squareRoot/sqrt.py
@@ -0,0 +1,17 @@
+# Square root of a number without using in-built exponent function or operators
+# Note: Truncate result to return only integer part
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        
+        """
+        result = x
+        while not result * result - x < 1:
+            result = (result + x / result) / 2
+
+        print(int(result))
+        return int(result)
+        
